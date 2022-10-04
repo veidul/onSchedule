2c2a872e871ee1ef9b3ff343bef9f59777b5cc12
ludie lambright <ludiefletcher@aol.com>
2022-10-04 13:42:09 -0700

    added hamburger drop down menu for mobile responsiveness

---
+++ pages/components/burgerMenu.js
@@ -0,0 +1,87 @@
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+
+const solutions = [
+  {
+    name: "Daily",
+    description: "Day view of events",
+    href: "/components/daily",
+  },
+  {
+    name: "Weekly",
+    description: "Week view of events",
+    href: "/components/weekly",
+  },
+  {
+    name: "Monthly",
+    description: "Month view of events",
+    href: "/components/monthly",
+  },
+  {
+    name: "About",
+    description: "Learn more about OnSchedule",
+    href: "/components/about",
+  },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function BurgerMenu() {
+  return (
+    <Popover className="relative">
+      {({ open }) => (
+        <>
+          <Popover.Button
+            className={classNames(
+              open ? "text-gray-900" : "text-gray-500",
+              "group inline-flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+            )}
+          >
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Solutions</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </Popover.Button>
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel className="absolute z-10 mt-3 w-screen max-w-xs -translate-x-72 transform px-2 sm:px-0">
+              <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                  {solutions.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-m-3 block rounded-md p-3 transition duration-150 ease-in-out hover:bg-gray-50"
+                    >
+                      <p className="text-base font-medium text-gray-900">
+                        {item.name}
+                      </p>
+                      <p className="mt-1 text-sm text-gray-500">
+                        {item.description}
+                      </p>
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </>
+      )}
+    </Popover>
+  );
+}
--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState } from "react";
+import BurgerMenu from "./burgerMenu";
 import LoginLogoutBtn from "./loginLogoutbtn";
 
 export default function Navbar() {
@@ -20,18 +21,9 @@ export default function Navbar() {
           </span>
         </div>
         <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
+          <BurgerMenu />
         </div>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="w-full block hidden flex-grow lg:flex lg:items-center lg:w-auto">
           <div className="text-sm lg:flex-grow">
             <a
               href="/components/daily"
